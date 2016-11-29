c41bb0462db86687cc72c82d219c4f447a9cdb88
Dhiggins94 <dhiggins94@gmail.com>
2016-11-29 13:46:00 -0500

    love is battlefield prototype 3

---
+++ Assets/scripts/hangry.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hangry : MonoBehaviour
+{
+    public GameObject fishpellet;
+    GameObject foodpelletclone;
+    Vector3 destination;
+    public float howHungry;
+    bool hungry;
+    public float spheresize = 10f;
+    // Use this for initialization
+    void Start()
+    {
+        hungry = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 destination = fishpellet.transform.position;
+        Vector3 direction = Vector3.Normalize(destination - transform.position);
+        Debug.Log("fish velocity =   " + GetComponent<Rigidbody>().velocity);
+        GetComponent<Rigidbody>().AddForce(direction * howHungry);
+        if (hungry)
+        {
+            destination = fishpellet.transform.position;
+
+         
+        }
+        else
+        {
+
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            hungry = true;
+        }
+    }
+
+
+    
+    void OnCollisionEnter(Collision co)
+    {
+
+
+        if (co.gameObject.tag == "fishpellet")
+        {
+           
+            
+            Vector3 nearbyposition = fishpellet.transform.position + Random.insideUnitSphere * spheresize;
+            GameObject clone = Instantiate(fishpellet, nearbyposition, Quaternion.identity) as GameObject;
+            GameObject temp = fishpellet;
+            Destroy(temp);
+            fishpellet = clone;
+            hungry = false;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+           
+            {
+
+                
+                }
+            }
+        }
+    }
---
+++ Assets/scripts/scott.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scott : MonoBehaviour
+{
+    public float scottstrength = 1000f;
+    public AudioClip hitsound;
+    public GameObject prefab1;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray beam = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(beam.origin, beam.direction * 1000f, Color.red);
+        RaycastHit beamhit = new RaycastHit();
+
+        if (Physics.Raycast(beam, out beamhit, 100f))
+        {
+            Debug.Log("hit?");
+
+
+
+            if (Input.GetMouseButton(0) && beamhit.rigidbody)
+            {
+                Debug.Log("Has rigibody");
+                beamhit.rigidbody.AddForce((Random.insideUnitSphere) * scottstrength);
+                beamhit.collider.gameObject.GetComponent<AudioSource>().PlayOneShot(hitsound);
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                Instantiate(prefab1, beamhit.point, Quaternion.identity);
+
+            }
+
+        }
+    }
+}
+
+    
+
