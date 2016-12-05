4ae34b99e43b54b07394c0ef87cfd851e50f92eb
Dhiggins94 <dhiggins94@gmail.com>
2016-12-04 21:28:08 -0500

    love is a battlefield prototype 3

---
+++ Assets/scripts/feelsbad.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class feelsbad: MonoBehaviour {
+
+    public int playerHealth = 30;
+    int damage = 10;
+
+	// Use this for initialization
+	void Start () {
+        print(playerHealth);
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision _collision) {
+	if (_collision.gameObject.tag == "enemy")
+        {
+            playerHealth -= damage;
+            if (playerHealth == 0)
+            {
+                gameOver();
+            }
+            print("i've been touched" + playerHealth);
+            }
+        }
+
+    void gameOver()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        print("its over isnt it");
+    }
+	}
+
---
+++ Assets/scripts/timer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class timer : MonoBehaviour {
+    public string levelToLoad;
+    public float timedown = 80f;
+    private Text timertext;
+    private bool timerisActive = true;
+
+	// Use this for initialization
+	void Start () {
+        timertext=GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timerisActive)
+        {
+        timedown -= Time.deltaTime;
+        timertext.text = timedown.ToString("f0");
+        print(timedown);
+            if (timedown <= 0)
+            {
+                timedown = 0;
+                timerisActive = false;
+                print("time's up");
+            }
+            }
+       
+        }
+	}
+
